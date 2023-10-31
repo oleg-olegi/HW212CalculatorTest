5616bd52f0bd7d6f3f225106b90229a378fc7c7a
oleg-olegi <ol.shink87@gmail.com>
2023-10-31 21:06:27 +0300

    Initial commit

---
+++ src/main/java/com/example/hw212calculatortest/Hw212CalculatorTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.hw212calculatortest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw212CalculatorTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hw212CalculatorTestApplication.class, args);
+    }
+
+}
